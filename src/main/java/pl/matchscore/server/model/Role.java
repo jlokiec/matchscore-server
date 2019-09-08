717909d57458483a61ca5d4988490bf43c1c2129
Jakub Łokieć <jakublokiec@wp.pl>
2019-09-08 14:15:50 +0200

    Create model for role entity

---
+++ src/main/java/pl/matchscore/server/model/Role.java
@@ -0,0 +1,39 @@
+package pl.matchscore.server.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String roleName;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+    public Role() {
+        users = new ArrayList<>();
+    }
+
+    public Role(String roleName) {
+        this.roleName = roleName;
+        users = new ArrayList<>();
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public void removeUser(User user) {
+        users.remove(user);
+    }
+}
