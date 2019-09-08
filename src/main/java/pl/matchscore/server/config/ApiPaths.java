28edf53c1ebafba3b85c91385b3dbfc9f8730994
Jakub Łokieć <jakublokiec@wp.pl>
2019-09-08 16:56:38 +0200

    Configure server context path and API paths

---
+++ src/main/java/pl/matchscore/server/config/ApiPaths.java
@@ -0,0 +1,7 @@
+package pl.matchscore.server.config;
+
+public class ApiPaths {
+    public static final String PUBLIC = "/public";
+    public static final String PUBLIC_ANT_MATCHER = PUBLIC + "/**";
+    public static final String AUTH = "/auth";
+}
