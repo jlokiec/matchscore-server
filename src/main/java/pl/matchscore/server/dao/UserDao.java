3e208369a700d518592595e98d9f3ed65c1f7255
Jakub Łokieć <jakublokiec@wp.pl>
2019-09-08 16:50:49 +0200

    Create DAO for user entity

---
+++ src/main/java/pl/matchscore/server/dao/UserDao.java
@@ -0,0 +1,8 @@
+package pl.matchscore.server.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.matchscore.server.models.User;
+
+public interface UserDao extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
