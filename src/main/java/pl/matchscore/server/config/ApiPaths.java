3411f5e8545334363d66fc9d0ab13849812fd9e7
Jakub Łokieć <jakublokiec@wp.pl>
2019-09-09 13:12:49 +0200

    Create open REST controller for league category

--- src/main/java/pl/matchscore/server/config/ApiPaths.java
+++ src/main/java/pl/matchscore/server/config/ApiPaths.java
@@ -4,4 +4,5 @@ public class ApiPaths {
     public static final String PUBLIC = "/public";
     public static final String PUBLIC_ANT_MATCHER = PUBLIC + "/**";
     public static final String AUTH = "/auth";
+    public static final String PUBLIC_LEAGUE_CATEGORIES_PATH = PUBLIC + "/categories";
 }
