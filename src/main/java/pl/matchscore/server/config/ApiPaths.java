1938efa586b186e7308b7d609ec88cf40afcb577
Jakub Łokieć <jakublokiec@wp.pl>
2019-09-11 14:23:22 +0200

    Create open REST controller for league
    getAll method allows to filter leagues by providing category id.

--- src/main/java/pl/matchscore/server/config/ApiPaths.java
+++ src/main/java/pl/matchscore/server/config/ApiPaths.java
@@ -5,4 +5,5 @@ public class ApiPaths {
     public static final String PUBLIC_ANT_MATCHER = PUBLIC + "/**";
     public static final String AUTH = "/auth";
     public static final String PUBLIC_LEAGUE_CATEGORIES_PATH = PUBLIC + "/categories";
+    public static final String PUBLIC_LEAGUES_PATH = PUBLIC + "/leagues";
 }
