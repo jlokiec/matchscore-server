bec88d3bfaa3235be43c11a378249b27c2d7ecf0
Jakub Łokieć <jakublokiec@wp.pl>
2019-09-12 15:10:21 +0200

    Perform code refactor in LeagueCategoryDao

--- src/main/java/pl/matchscore/server/dao/LeagueCategoryDao.java
+++ src/main/java/pl/matchscore/server/dao/LeagueCategoryDao.java
@@ -3,6 +3,6 @@ package pl.matchscore.server.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.matchscore.server.models.LeagueCategory;
 
-public interface LeagueCategoryDao extends JpaRepository<LeagueCategory,Long> {
+public interface LeagueCategoryDao extends JpaRepository<LeagueCategory, Long> {
 
 }
