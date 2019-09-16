c547ae2bb75d2a58b3a3cbd9e79531625006d1fd
Jakub Łokieć <jakublokiec@wp.pl>
2019-09-16 12:19:51 +0200

    Improve code formatting in User entity class

--- src/main/java/pl/matchscore/server/models/User.java
+++ src/main/java/pl/matchscore/server/models/User.java
@@ -31,11 +31,8 @@ public class User {
     private boolean enabled;
 
     @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-            name = "user_roles",
-            joinColumns = @JoinColumn(
-                    name = "user_id", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(
-                    name = "role_id", referencedColumnName = "id"))
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
     private List<Role> roles;
 }
