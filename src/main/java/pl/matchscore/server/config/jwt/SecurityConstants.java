b0a1bc5ac0ab056689387a7ee938d35fbd717683
Jakub Łokieć <jakublokiec@wp.pl>
2019-09-19 22:26:57 +0200

    Add method for creating cookie with JWT

--- src/main/java/pl/matchscore/server/config/jwt/SecurityConstants.java
+++ src/main/java/pl/matchscore/server/config/jwt/SecurityConstants.java
@@ -1,7 +1,9 @@
 package pl.matchscore.server.config.jwt;
 
 public final class SecurityConstants {
-    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final int COOKIE_MAX_AGE = 24 * 60 * 60;
+    public static final String COOKIE_NAME = "matchscore_jwt";
+    public static final String COOKIE_PATH = "/api";
     public static final long TOKEN_EXPIRATION_MILLIS = 24 * 60 * 60 * 1000;
     public static final String TOKEN_ISSUER = "matchscore-api";
     public static final String TOKEN_AUDIENCE = "matchscore-client";
