fc8bad129c19bdffbe2f7edac9602fe0e2ec6b63
Jakub Łokieć <jakublokiec@wp.pl>
2019-09-27 19:54:03 +0200

    Add confirm method to RegisterUserController

--- src/main/java/pl/matchscore/server/controllers/open/RegisterUserController.java
+++ src/main/java/pl/matchscore/server/controllers/open/RegisterUserController.java
@@ -2,14 +2,12 @@ package pl.matchscore.server.controllers.open;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.matchscore.server.config.ApiPaths;
 import pl.matchscore.server.models.dto.UserRegistrationDto;
 import pl.matchscore.server.services.UserService;
 import pl.matchscore.server.services.exceptions.EmailTakenException;
+import pl.matchscore.server.services.exceptions.UserNotFoundException;
 import pl.matchscore.server.services.exceptions.UsernameTakenException;
 
 import javax.servlet.http.HttpServletResponse;
@@ -30,6 +28,15 @@ public class RegisterUserController {
         try {
             service.register(userDto);
         } catch (UsernameTakenException | EmailTakenException e) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+    @GetMapping(value = "/confirm")
+    public void confirm(@RequestParam String uuid, HttpServletResponse response) {
+        try {
+            service.completeRegistration(uuid);
+        } catch (UserNotFoundException e) {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         }
     }
