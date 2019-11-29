1ab28d0f378c1c50bae0602deca47d2c2d56f3a9
Jakub Łokieć <jakublokiec@wp.pl>
2019-11-29 16:18:09 +0100

    Create service for rate report

---
+++ src/main/java/pl/matchscore/server/models/dto/ReportRatingDto.java
@@ -0,0 +1,21 @@
+package pl.matchscore.server.models.dto;
+
+import lombok.Data;
+import pl.matchscore.server.models.ReportRating;
+
+@Data
+public class ReportRatingDto {
+    private long id;
+    private long reportId;
+    private int rating;
+    private String ratedBy;
+    private long ratingTimestamp;
+
+    public ReportRatingDto(ReportRating reportRating) {
+        this.id = reportRating.getId();
+        this.reportId = reportRating.getReport().getId();
+        this.rating = reportRating.getRating();
+        this.ratedBy = reportRating.getRatedBy().getUsername();
+        this.ratingTimestamp = reportRating.getRatingTimestamp();
+    }
+}
