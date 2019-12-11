57cf842de702889d970641104146411541d3da59
Jakub Łokieć <jakublokiec@wp.pl>
2019-12-11 14:31:44 +0100

    Add reputation to user

--- src/main/java/pl/matchscore/server/models/User.java
+++ src/main/java/pl/matchscore/server/models/User.java
@@ -41,4 +41,6 @@ public class User {
             joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
     private List<Role> roles;
+
+    private int reputation;
 }
